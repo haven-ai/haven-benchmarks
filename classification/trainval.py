26af4550df2e942999494e1bafd2dbd8aa27363e
liuzheyu1998 <zheyu.liu@mail.mcgill.ca>
2020-09-26 21:11:52 -0400

    Changed config to configs

---
+++ classification/trainval.py
@@ -0,0 +1,19 @@
+from haven import haven_chk as hc
+from haven import haven_results as hr
+from haven import haven_utils as hu
+import torch
+import torchvision
+import tqdm
+import pandas as pd
+import pprint
+import itertools
+import os
+import pylab as plt
+import exp_configs
+import time
+import numpy as np
+
+from src import models
+from src import datasets
+
+import argparse
