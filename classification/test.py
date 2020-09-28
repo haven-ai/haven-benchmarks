6262b76e8296420cf40d326bfa66ca2b17a9ea3b
Issam Laradji <issamou@cedar1.cedar.computecanada.ca>
2020-09-28 06:47:18 -0700

    added parse args

--- classification/test.py
+++ classification/test.py
@@ -1,1 +1,9 @@
-print('hello world')
+import argparse 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--name', required=True)
+
+    args = parser.parse_args()
+
+    print('hello %s' % args.name)
