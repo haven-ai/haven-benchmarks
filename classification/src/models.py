aecd8a1bcce180964bffd94448f62d226e1b22be
liuzheyu1998 <zheyu.liu@mail.mcgill.ca>
2020-10-04 15:35:37 -0400

    Update models.py

--- classification/src/models.py
+++ classification/src/models.py
@@ -24,6 +24,16 @@ class Mlp(nn.Module):
         self.hidden_layers = nn.ModuleList([nn.Linear(in_size, out_size, bias=bias) for
                                             in_size, out_size in zip([self.input_size] + hidden_sizes[:-1], hidden_sizes)])
         self.output_layer = nn.Linear(hidden_sizes[-1], n_classes, bias=bias)
+        if self.exp_dict["optimizer"] == "adam":
+            self.opt = torch.optim.Adam(
+                self.model_base.parameters(), lr=self.exp_dict["lr"], betas=(0.99, 0.999), weight_decay=0.0005)
+
+        elif self.exp_dict["optimizer"] == "sgd":
+            self.opt = torch.optim.SGD(
+                self.model_base.parameters(), lr=self.exp_dict["lr"])
+
+        else:
+            raise ValueError
 
     def forward(self, x):
         x = x.view(-1, self.input_size)
@@ -38,3 +48,77 @@ class Mlp(nn.Module):
         logits = self.output_layer(out)
 
         return logits
+
+
+
+    def train_on_loader(model, train_loader):
+        model.train()
+
+        n_batches = len(train_loader)
+        train_meter = Meter()
+
+        pbar = tqdm.tqdm(total=n_batches)
+        for batch in train_loader:
+            score_dict = model.train_on_batch(batch)
+            train_meter.add(score_dict['train_loss'], batch['images'].shape[0])
+
+            pbar.set_description("Training. Loss: %.4f" % train_meter.get_avg_score())
+            pbar.update(1)
+
+        pbar.close()
+
+        return {'train_loss': train_meter.get_avg_score()}
+
+    @torch.no_grad()
+    def val_on_loader(self, val_loader, savedir_images=None, n_images=2):
+        self.eval()
+
+        n_batches = len(val_loader)
+        val_meter = Meter()
+        pbar = tqdm.tqdm(total=n_batches)
+        for i, batch in enumerate(tqdm.tqdm(val_loader)):
+            score_dict = self.val_on_batch(batch)
+            val_meter.add(score_dict['miscounts'], batch['images'].shape[0])
+            pbar.update(1)
+
+            if savedir_images and i < n_images:
+                os.makedirs(savedir_images, exist_ok=True)
+                self.vis_on_batch(batch, savedir_image=os.path.join(
+                    savedir_images, "%d.jpg" % i))
+
+                pbar.set_description("Validating. MAE: %.4f" % val_meter.get_avg_score())
+
+        pbar.close()
+        val_mae = val_meter.get_avg_score()
+        val_dict = {'val_mae': val_mae, 'val_score': -val_mae}
+        return val_dict
+
+    def train_on_batch(self, batch, **extras):
+        self.opt.zero_grad()
+        self.train()
+
+        images = batch["images"].cuda()
+        points = batch["points"].long().cuda()
+        logits = forward(images)
+        criterion = torch.nn.CrossEntropyLoss(reduction="mean")
+        loss = criterion(logits, points.view(-1))
+        loss.backward()
+
+        self.opt.step()
+
+        return {"train_loss": loss.item()}
+
+
+
+
+class Meter:
+    def __init__(self):
+        self.n_sum = 0.
+        self.n_counts = 0.
+
+    def add(self, n_sum, n_counts):
+        self.n_sum += n_sum
+        self.n_counts += n_counts
+
+    def get_avg_score(self):
+        return self.n_sum / self.n_counts
