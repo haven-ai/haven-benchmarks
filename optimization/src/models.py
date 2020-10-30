da233a41ab172b17b996e2319f8b4ce2e16fcb24
Xinhong Deng <xhdeng@cedar5.cedar.computecanada.ca>
2020-10-29 22:42:21 -0700

    add new models and factor out train and validate

--- optimization/src/models.py
+++ optimization/src/models.py
@@ -1,5 +1,5 @@
 import os
-
+import math
 import torch.optim
 from torch import nn
 from torch.nn import functional as F
@@ -7,10 +7,17 @@ import tqdm
 import numpy as np
 
 
-def get_model(exp_dict):
+def get_model(exp_dict, train_set=None):
     model = None
     if exp_dict["model"]["name"] == "mlp":
         model = Mlp(n_classes=10, dropout=False)
+    elif exp_dict["model"]["name"] == "logistic":
+        batch = train_set[0]
+        model = Mlp(input_size=batch['images'].shape[0], hidden_sizes=[], n_classes=2, bias=False)
+    elif exp_dict["model"]["name"] == "densenet121":
+        model = DenseNet121(num_classes=10)
+    elif exp_dict["model"]["name"] == "resnet34":
+        model = ResNet([3, 4, 6, 3], num_classes=10)
     else:
         raise ValueError
 
@@ -28,6 +35,166 @@ def get_model(exp_dict):
 
 
 # =====================================================
+# Resnet
+class ResNet(nn.Module):
+    def __init__(self, num_blocks, num_classes=10):
+        super().__init__()
+        block = BasicBlock
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(
+            3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes,
+            planes,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_planes,
+                    self.expansion * planes,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False), nn.BatchNorm2d(self.expansion * planes))
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+# =====================================================
+# Denesnet121
+class Transition(nn.Module):
+    def __init__(self, in_planes, out_planes):
+        super(Transition, self).__init__()
+        self.bn = nn.BatchNorm2d(in_planes)
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False)
+
+    def forward(self, x):
+        out = self.conv(F.relu(self.bn(x)))
+        out = F.avg_pool2d(out, 2)
+        return out
+
+
+class DenseNet(nn.Module):
+    def __init__(self, block, nblocks, growth_rate=12, reduction=0.5, num_classes=10):
+        super().__init__()
+        self.growth_rate = growth_rate
+
+        num_planes = 2*growth_rate
+        self.conv1 = nn.Conv2d(
+            3, num_planes, kernel_size=3, padding=1, bias=False)
+
+        self.dense1 = self._make_dense_layers(block, num_planes, nblocks[0])
+        num_planes += nblocks[0]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans1 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense2 = self._make_dense_layers(block, num_planes, nblocks[1])
+        num_planes += nblocks[1]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans2 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense3 = self._make_dense_layers(block, num_planes, nblocks[2])
+        num_planes += nblocks[2]*growth_rate
+        out_planes = int(math.floor(num_planes*reduction))
+        self.trans3 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense4 = self._make_dense_layers(block, num_planes, nblocks[3])
+        num_planes += nblocks[3]*growth_rate
+
+        self.bn = nn.BatchNorm2d(num_planes)
+        self.linear = nn.Linear(num_planes, num_classes)
+
+    def _make_dense_layers(self, block, in_planes, nblock):
+        layers = []
+        for i in range(nblock):
+            layers.append(block(in_planes, self.growth_rate))
+            in_planes += self.growth_rate
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.trans1(self.dense1(out))
+        out = self.trans2(self.dense2(out))
+        out = self.trans3(self.dense3(out))
+        out = self.dense4(out)
+        out = F.avg_pool2d(F.relu(self.bn(out)), 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+class Bottleneck_DenseNet(nn.Module):
+    def __init__(self, in_planes, growth_rate):
+        super().__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, 4*growth_rate,
+                               kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(4*growth_rate)
+        self.conv2 = nn.Conv2d(4*growth_rate, growth_rate,
+                               kernel_size=3, padding=1, bias=False)
+
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = self.conv2(F.relu(self.bn2(out)))
+        out = torch.cat([out, x], 1)
+        return out
+
+
+def DenseNet121(num_classes):
+    return DenseNet(Bottleneck_DenseNet, [6, 12, 24, 16], growth_rate=32,
+                    num_classes=num_classes)
+
+
+# =====================================================
 # MLP
 class Mlp(nn.Module):
     def __init__(self, input_size=784,
@@ -36,9 +203,7 @@ class Mlp(nn.Module):
                  bias=True, dropout=False):
         super().__init__()
 
-        self.opt = None
-
-        self.dropout=dropout
+        self.dropout = dropout
         self.input_size = input_size
         self.hidden_layers = nn.ModuleList([nn.Linear(in_size, out_size, bias=bias) for
                                             in_size, out_size in zip([self.input_size] + hidden_sizes[:-1], hidden_sizes)])
@@ -58,87 +223,6 @@ class Mlp(nn.Module):
 
         return logits
 
-    def train_on_loader(model, train_loader):
-        model.train()
-
-        n_batches = len(train_loader)
-        train_meter = Meter()
-
-        pbar = tqdm.tqdm(total=n_batches)
-        for batch in train_loader:
-            images, _ = batch
-            score_dict = model.train_on_batch(batch)
-            train_meter.add(score_dict['train_loss'], images.shape[0])
-
-            pbar.set_description("Training. Loss: %.4f" % train_meter.get_avg_score())
-            pbar.update(1)
-
-        pbar.close()
-
-        return {'train_loss': train_meter.get_avg_score()}
-
-    @torch.no_grad()
-    def val_on_loader(self, val_loader, savedir_images=None, n_images=2):
-        # todo: save dir? n_image?
-        self.eval()
-
-        n_batches = len(val_loader)
-        val_meter = Meter()
-        pbar = tqdm.tqdm(total=n_batches)
-        # for i, batch in enumerate(tqdm.tqdm(val_loader)):
-        for i, batch in enumerate(val_loader):
-            images, _ = batch
-            score_dict = self.val_on_batch(batch)
-            val_meter.add(score_dict['success'], images.shape[0])
-            self.val_on_batch(batch)
-
-            pbar.set_description("Validating. %.4f acc" % val_meter.get_avg_score())
-            pbar.update(1)
-
-            # if savedir_images and i < n_images:
-            #     os.makedirs(savedir_images, exist_ok=True)
-            #     self.val_on_batch(batch)
-            #
-            #     pbar.set_description("Validating. MAE: %.4f" % val_meter.get_avg_score())
-
-        pbar.close()
-        val_acc = val_meter.get_avg_score()
-        val_dict = {'val_acc': val_acc}
-        return val_dict
-
-    def train_on_batch(self, batch, **extras):
-        self.opt.zero_grad()
-        self.train()
-
-        images, labels = batch
-        logits = self.forward(images)
-        criterion = torch.nn.CrossEntropyLoss(reduction="mean")
-        loss = criterion(logits, labels.view(-1))
-        loss.backward()
-
-        self.opt.step()
-
-        return {"train_loss": loss.item()}
-
-    def val_on_batch(self, batch):
-        self.eval()
-        images, labels = batch
-        logits = self.forward(images)
-        probs = logits.sigmoid().cpu().numpy()
-
-        classifications = np.argmax(probs,axis=1)
-        return {'success': (classifications == labels.numpy()).sum()}
-
-    def get_state_dict(self):
-        state_dict = {"model": self.state_dict(),
-                      "opt":self.opt.state_dict()}
-
-        return state_dict
-        
-    def set_state_dict(self, state_dict):
-        self.load_state_dict(state_dict["model"])
-        self.opt.load_state_dict(state_dict["opt"])
-
 
 class Meter:
     def __init__(self):
@@ -151,3 +235,4 @@ class Meter:
 
     def get_avg_score(self):
         return self.n_sum / self.n_counts
+
