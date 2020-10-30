c80f17e6309f1f8223de8f3f23f51624f79465c8
Xinhong Deng <xhdeng@cedar5.cedar.computecanada.ca>
2020-10-29 21:38:06 -0700

    config file

--- optimization/exp_configs.py
+++ optimization/exp_configs.py
@@ -1,24 +1,53 @@
 from haven import haven_utils as hu
-
+RUNS = [0, 1, 2]
+opt_list = ['sgd', 'adam', 'adaptive_second', 'sps', 'adaptive_first']
 EXP_GROUPS = {}
-EXP_GROUPS['mnist_batch_size'] = {"dataset": {'name': 'mnist'},
-                                  "model": {'name': 'mlp'},
-                                  "batch_size": [64, 128, 256, 512, 1024],
-                                  "max_epoch": [100],
-                                  'dataset_size': [
-                                      {'train': 'all', 'val': 'all'},
-                                  ],
-                                  'optimizer': ['adam'],
-                                  'lr': [1e-5]
-                                  }
-EXP_GROUPS['mnist_learning_rate'] = {"dataset": {'name': 'mnist'},
-                                  "model": {'name': 'mlp'},
-                                  "batch_size": [256],
-                                  "max_epoch": [100],
-                                  'dataset_size': [
-                                      {'train': 'all', 'val': 'all'},
-                                  ],
-                                  'optimizer': ['adam'],
-                                  'lr': [5e-6, 1e-5, 5e-5, 1e-4, 5e-4]
-                                  }
-EXP_GROUPS = {k: hu.cartesian_exp_group(v) for k, v in EXP_GROUPS.items()}
+
+def get_benchmark(benchmark, opt_list=opt_list):
+    if benchmark == 'mnist':
+        exp = {"dataset": {'name': 'mnist'},
+                "model": {'name': 'mlp'},
+                "runs": RUNS,
+                "batch_size": [128],
+                "max_epoch": [100],
+                'dataset_size': [
+                    {'train': 'all', 'val': 'all'},
+                ],
+                "loss_func": ["softmax_loss"],
+                "opt": opt_list,
+                "acc_func": ["softmax_accuracy"],
+                }
+        EXP_GROUPS = hu.cartesian_exp_group(exp)
+    elif benchmark == 'syn':
+        exp = {"dataset": {'name': 'synthetic'},
+                "model": {'name': 'logistic'},
+                "runs": RUNS,
+                "batch_size": [128],
+                "max_epoch": [200],
+                'dataset_size': [
+                    {'train': 'all', 'val': 'all'},
+                ],
+                "loss_func": ["softmax_loss"],
+                "opt": opt_list,
+                "acc_func": ["softmax_accuracy"],
+                'margin': [0.05, 0.1, 0.5, 0.01],
+                "n_samples": [1000],
+                "d": 20
+                }
+        EXP_GROUPS = hu.cartesian_exp_group(exp)
+        
+    elif benchmark == 'cifar10':
+        exp = {"dataset": {'name': 'synthetic'},
+                "model": {'name': ["densenet121", "resnet34"]},
+                "runs": RUNS,
+                "batch_size": [128],
+                "max_epoch": [200],
+                'dataset_size': [
+                    {'train': 'all', 'val': 'all'},
+                ],
+                "loss_func": ["softmax_loss"],
+                "opt": opt_list,
+                "acc_func": ["softmax_accuracy"]
+                }
+        EXP_GROUPS = hu.cartesian_exp_group(exp)
+        
