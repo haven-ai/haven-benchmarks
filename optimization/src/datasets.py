32d8b01a42ca3472ff08a4f6ef8b950b416bf1f9
Xinhong Deng <xhdeng@cedar5.cedar.computecanada.ca>
2020-10-29 21:51:10 -0700

    update dataset

--- optimization/src/datasets.py
+++ optimization/src/datasets.py
@@ -3,7 +3,11 @@ from sklearn.model_selection import train_test_split
 from torchvision import transforms
 import torch
 from sklearn import metrics
+from sklearn.svm import SVC
+from torch.utils.data import Dataset
+import tqdm
 from haven import haven_utils as hu
+import numpy as np
 
 def get_dataset(dataset_name, train_flag, datadir, exp_dict):
     if dataset_name == "mnist":
@@ -14,4 +18,127 @@ def get_dataset(dataset_name, train_flag, datadir, exp_dict):
                                    torchvision.transforms.Normalize(
                                        (0.5,), (0.5,))
                                ]))
+    
+    if dataset_name == "cifar10":
+        transform_function = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+
+        dataset = torchvision.datasets.CIFAR10(
+            root=datadir,
+            train=train_flag,
+            download=True,
+            transform=transform_function)
+
+    if dataset_name == "synthetic":
+        margin = exp_dict["margin"]
+
+        X, y, _, _ = make_binary_linear(n=exp_dict["n_samples"],
+                                        d=exp_dict["d"],
+                                        margin=margin,
+                                        y01=True,
+                                        bias=True,
+                                        separable=exp_dict.get("separable", True),
+                                        seed=42)
+        # No shuffling to keep the support vectors inside the training set
+        splits = train_test_split(X, y, test_size=0.2, shuffle=False, random_state=42)
+        X_train, X_test, Y_train, Y_test = splits
+
+        X_train = torch.FloatTensor(X_train)
+        X_test = torch.FloatTensor(X_test)
+
+        Y_train = torch.LongTensor(Y_train)
+        Y_test = torch.LongTensor(Y_test)
+
+        if train_flag:
+            dataset = torch.utils.data.TensorDataset(X_train, Y_train)
+        else:
+            dataset = torch.utils.data.TensorDataset(X_test, Y_test)
+
     return dataset
+
+
+def make_binary_linear(n, d, margin, y01=False, bias=False, separable=True, scale=1, shuffle=True, seed=None):
+    assert margin >= 0.
+
+    if seed:
+        np.random.seed(seed)
+
+    labels = [-1, 1]
+
+    # Generate support vectors that are 2 margins away from each other
+    # that is also linearly separable by a homogeneous separator
+    w = np.random.randn(d); w /= np.linalg.norm(w)
+    # Now we have the normal vector of the separating hyperplane, generate
+    # a random point on this plane, which should be orthogonal to w
+    p = np.random.randn(d-1); l = (-p@w[:d-1])/w[-1]
+    p = np.append(p, [l])
+
+    # Now we take p as the starting point and move along the direction of w
+    # by m and -m to obtain our support vectors
+    v0 = p - margin*w
+    v1 = p + margin*w
+    yv = np.copy(labels)
+
+    # Start generating points with rejection sampling
+    X = []; y = []
+    for i in range(n-2):
+        s = scale if np.random.random() < 0.05 else 1
+
+        label = np.random.choice(labels)
+        # Generate a random point with mean at the center 
+        xi = np.random.randn(d)
+        xi = (xi / np.linalg.norm(xi))*s
+
+        dist = xi@w
+        while dist*label <= margin:
+            u = v0-v1 if label == -1 else v1-v0
+            u /= np.linalg.norm(u)
+            xi = xi + u
+            xi = (xi / np.linalg.norm(xi))*s
+            dist = xi@w
+
+        X.append(xi)
+        y.append(label)
+
+    X = np.array(X).astype(float); y = np.array(y)#.astype(float)
+
+    if shuffle:
+        ind = np.random.permutation(n-2)
+        X = X[ind]; y = y[ind]
+
+    # Put the support vectors at the beginning
+    X = np.r_[np.array([v0, v1]), X]
+    y = np.r_[np.array(yv), y]
+
+    if separable:
+        # Assert linear separability
+        # Since we're supposed to interpolate, we should not regularize.
+        clff = SVC(kernel="linear", gamma="auto", tol=1e-10, C=1e10)
+        clff.fit(X, y)
+        assert clff.score(X, y) == 1.0
+
+        # Assert margin obtained is what we asked for
+        w = clff.coef_.flatten()
+        sv_margin = np.min(np.abs(clff.decision_function(X)/np.linalg.norm(w)))
+        
+        if np.abs(sv_margin - margin) >= 1e-4:
+            print("Prescribed margin %.4f and actual margin %.4f differ (by %.4f)." % (margin, sv_margin, np.abs(sv_margin - margin)))
+
+    else:
+        flip_ind = np.random.choice(n, int(n*0.01))
+        y[flip_ind] = -y[flip_ind]
+
+    if y01:
+        y[y==-1] = 0
+
+    if bias:
+        # TODO: Get rid of this later, bias should be handled internally,
+        #       this is just for ease of implementation for the Hessian
+        X = np.c_[np.ones(n), X]
+
+    return X, y, w, (v0, v1)
